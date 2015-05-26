787fbe58ff3fa15c9868b96806ec48f025d1ffde
Neeraj Gangwar <neeraj.gangwar@commonfloor.com>
2015-05-26 23:26:41 +0530

    add: adding docker related build statements

--- project/GluonBuild.scala
+++ project/GluonBuild.scala
@@ -8,13 +8,23 @@ import com.typesafe.sbt.SbtMultiJvm.MultiJvmKeys.{ MultiJvm, extraOptions, jvmOp
 import com.typesafe.sbt.SbtScalariform
 import com.typesafe.sbt.SbtScalariform.ScalariformKeys
 
-import com.typesafe.sbt.SbtStartScript
+// import com.typesafe.sbt.SbtStartScript
+import com.typesafe.sbt.packager.archetypes.JavaAppPackaging
 
 import sbtassembly.AssemblyPlugin.autoImport._
 
 import com.twitter.scrooge.ScroogeSBT
 
+import org.apache.maven.artifact.handler.DefaultArtifactHandler
+
+import com.typesafe.sbt.SbtNativePackager._, autoImport._
+import com.typesafe.sbt.packager.Keys._
+import com.typesafe.sbt.packager.docker.{Cmd, ExecCmd, CmdLike}
+
+
 object GluonBuild extends Build with Libraries {
+
+
   def sharedSettings = Seq(
     organization := "com.goshoplane",
     version := "0.0.1",
@@ -63,12 +73,12 @@ object GluonBuild extends Build with Libraries {
       ++ Libs.scaldi
   )
 
-    lazy val catalogue = Project(
+  lazy val catalogue = Project(
     id = "gluon-catalogue",
     base = file("catalogue"),
     settings = Project.defaultSettings ++
-      sharedSettings ++
-      SbtStartScript.startScriptForClassesSettings
+      sharedSettings //++
+      // SbtStartScript.startScriptForClassesSettings
   ).settings(
     name := "gluon-catalogue",
 
@@ -89,15 +99,34 @@ object GluonBuild extends Build with Libraries {
       ++ Libs.catalogueCommons
   ).dependsOn(core)
 
-    lazy val service = Project(
+  lazy val service = Project(
     id = "gluon-service",
     base = file("service"),
     settings = Project.defaultSettings ++
-      sharedSettings ++
-      SbtStartScript.startScriptForClassesSettings
-  ).settings(
+      sharedSettings
+      // SbtStartScript.startScriptForClassesSettings
+  ).enablePlugins(JavaAppPackaging)
+   .settings(
     name := "gluon-service",
+    mainClass in Compile := Some("gluon.service.GluonServer"),
 
+    dockerExposedPorts := Seq(2106),
+    // TODO: remove echo statement once verified
+    dockerEntrypoint := Seq("sh", "-c", "export GLUON_HOST=`/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1 }'` && echo $GLUON_HOST && bin/gluon-service $*"),
+    dockerRepository := Some("docker"),
+    dockerBaseImage := "phusion/baseimage",
+    dockerCommands ++= Seq(
+      Cmd("USER", "root"),
+      new CmdLike {
+        def makeContent = """|RUN \
+                             |  echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | debconf-set-selections && \
+                             |  add-apt-repository -y ppa:webupd8team/java && \
+                             |  apt-get update && \
+                             |  apt-get install -y oracle-java7-installer && \
+                             |  rm -rf /var/lib/apt/lists/* && \
+                             |  rm -rf /var/cache/oracle-jdk7-installer""".stripMargin
+      }
+    ),
     libraryDependencies ++= Seq(
     ) ++ Libs.akka
       ++ Libs.slf4j
